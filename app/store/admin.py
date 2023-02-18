bfb36ee82a73327598245ec139919b0a1a5d8b45
Ruslan Zulufov <gothiq2302@gmail.com>
2023-02-18 12:14:59 +0600

    Создал модели, добавил поля в админку

--- app/store/admin.py
+++ app/store/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from store.models import Category, Product
+
+
 # Register your models here.
+@admin.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'created_at', 'updated_at')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'price', 'created_at', 'updated_at')
--- app/store/models.py
+++ app/store/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False, verbose_name="Наименование")
+    description = models.CharField(max_length=300, null=True, blank=False, verbose_name="Описание")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата и время создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата и время обновления")
+
+    def __str__(self):
+        return f"{self.title} - {self.created_at}"
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False, verbose_name="Наименование")
+    description = models.CharField(max_length=300, null=True, blank=False, verbose_name="Описание")
+    category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Стоимость")
+    image = models.CharField(max_length=300, verbose_name="Картинка")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата и время создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата и время обновления")
+
+    def __str__(self):
+        return f"{self.category} - {self.title}"
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
